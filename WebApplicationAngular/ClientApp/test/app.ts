aed64b64dcb83f98e3e0f434c6c5bc4de5d3066c
Gabriel Barros de Paula <gabriel_ciac6de@hotmail.com>
2020-01-28 16:41:57 -0300

    Importando uma biblioteca JavaScript com tipos.

--- WebApplicationAngular/ClientApp/test/app.ts
+++ WebApplicationAngular/ClientApp/test/app.ts
@@ -1,6 +1,10 @@
 import { SpaceCraft, Containership } from "./base-ships";
 import { MillenniumFalcon } from "./starfighters";
 
+import * as _ from 'lodash';
+
+console.log(_.pad("Typescript Examples", 40, "="));
+
 let ship = new SpaceCraft('hyperdrive');
 ship.jumpIntoHyperspace();
 
